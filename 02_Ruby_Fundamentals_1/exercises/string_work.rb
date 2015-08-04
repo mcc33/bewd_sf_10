1f623c76fd80257804e0f06839b89bef19bfbe6a
Mike <cassaro.mike@gmail.com>
2015-08-03 20:19:39 -0700

    string_work completed

---
+++ 02_Ruby_Fundamentals_1/exercises/string_work.rb
@@ -0,0 +1,12 @@
+game = "Monopoly"
+food = "Cheese Pie"
+name = "Mike"
+sport = "Baseball"
+drink = "       water"
+
+puts game.length
+puts food.prepend(" I like ")
+puts name << "C"
+puts drink.strip
+puts sport.chars
+puts sport.downcase
